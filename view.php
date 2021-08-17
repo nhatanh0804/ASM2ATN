3dc04fbba3be4c8a64d0ddd6bceac5139f1874f5
Nguyen Nhat Anh <anhnnbhaf190127@fpt.edu.vn>
2021-08-17 14:53:31 +0700

    git init

--- view.php
+++ view.php
@@ -1,26 +1,26 @@
 <!doctype html>
-<html>
-<head>
- <meta charset="UTF-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>Document</title>
-</head>
-<body>
-<style type="text/css">
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Document</title>
+	</head>
+	<body>
+		<style type="text/css">
 		#h_menu {
 			width: 1000px;
 			line-height: 30px;
-		    background-color: blue
-				
+			background-color: blue
+			
 		}
 		#h_menu ul li{
 			
@@ -45,7 +45,7 @@
 			background-color: aqua;
 			float: left;
 			border: 1px solid black;
-		
+			
 		}
 		.content{
 			width: 100%;
@@ -82,122 +82,127 @@
 			margin-left: 30px;
 			padding: 10px;
 		}
-		}
-		table{
-			border: 1px solid black;
-		}
-	</style>
+	}
+	table{
+		border: 1px solid black;
+	}
+</style>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="../Index.php">ATN</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+	<a class="navbar-brand" href="../Index.php">ATN</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-     
-		<li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Admin
-        </a>
-              </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Account
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="Dangnhap.php">Sign In</a>
-          <a class="dropdown-item" href="Dangky.php">Sign Up</a>
-        
-        </div>
-      </li>
-
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-    <div class="container">
-        <div class="row justify-content-center">
-			<div class='col-2'>
-				<h6>Song Name</h6>
-			</div>
-			<div class='col-2'>
-				<h6>Price</h6>
-			</div>
-			<div class='col-2'>
-				<h6>Image</h6>
-			</div>
-			<div class='col-2'>
-				<h6>Genre</h6>
-			</div>
-			<div class='col-2'>
-				<h6>Action</h6>
-			</div>
-		</div>
-		<?php 
-		$conn = mysqli_connect('localhost','root','','asm2');
-		$sql="select*from product";
-		$result= $conn->query($sql);
-		while($song=$result->fetch_object()){
-			echo "
-        <div class='row justify-content-center' style='margin-bottom:20px;'>
-			<div class='col-2'>
-				<p>$song->ProductName</p>
-			</div>
-			<div class='col-2'>
-				<p>$song->Price</p>
-			</div>
-			<div class='col-2'>
-				<img src='img/$song->ProductImage' style=' height: 60px; width: 60px;' >
-			</div>";
-			$genreID=$song->CategoryID;
-			$sql1="select*from category where CategoryID=$genreID";
-			$result1= $conn->query($sql1);
-			$category=$result1->fetch_object();
-			echo"
-			<div class='col-2'>
-				<p>$category->CategoryName</p>
-			</div>
-			<div class='col-2'>
-			    <a href='Add.php'>Add</a>
-				<a href='Edit.php?update_id=$song->ProductID'>Edit</a>
-				<a href='view.php?delete_id=$song->ProductID'>Delete</a>
-			</div>
-		</div>";
-		}
-			?>
-
-
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+			</li>
 			
-            
-
-    </div>
-                <?php
-                if(isset($_GET['delete_id'])){
-						
-					$songID=$_GET['delete_id'];
-                    
-                        $result2=$conn->query("delete from product where ProductID=$songID");
-                        
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Admin
+				</a>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Account
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="Dangnhap.php">Sign In</a>
+					<a class="dropdown-item" href="Dangky.php">Sign Up</a>
 					
-                        if($result2){
-                            echo "<script>alert('xóa dữ liệu thành công')</script>";
-							echo"<script>window.open('view.php','_self')</script>";
-                       
-                        }
-                        else{
-                            echo "<script>alert('xóa dữ liệu thất bại')</script>";
-                          
-                        }
-                }
-           
+				</div>
+			</li>
+
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		</form>
+	</div>
+</nav>
+<div class="container">
+	<div class="row justify-content-center">
+		<div class='col-2'>
+			<h6>Song Name</h6>
+		</div>
+		<div class='col-2'>
+			<h6>Price</h6>
+		</div>
+		<div class='col-2'>
+			<h6>Image</h6>
+		</div>
+		<div class='col-2'>
+			<h6>Genre</h6>
+		</div>
+		<div class='col-2'>
+			<h6>Action</h6>
+		</div>
+	</div>
+	<?php 
+	include("connect.php");
+	if (!$connect){
+		echo "kết nối thất bại";
+	}else{
+		echo "";
+	}
+	$sql="select*from product";
+	$result= $conn->query($sql);
+	while($song=$result->fetch_object()){
+		echo "
+		<div class='row justify-content-center' style='margin-bottom:20px;'>
+		<div class='col-2'>
+		<p>$song->ProductName</p>
+		</div>
+		<div class='col-2'>
+		<p>$song->Price</p>
+		</div>
+		<div class='col-2'>
+		<img src='img/$song->ProductImage' style=' height: 60px; width: 60px;' >
+		</div>";
+		$genreID=$song->CategoryID;
+		$sql1="select*from category where CategoryID=$genreID";
+		$result1= $conn->query($sql1);
+		$category=$result1->fetch_object();
+		echo"
+		<div class='col-2'>
+		<p>$category->CategoryName</p>
+		</div>
+		<div class='col-2'>
+		<a href='Add.php'>Add</a>
+		<a href='Edit.php?update_id=$song->ProductID'>Edit</a>
+		<a href='view.php?delete_id=$song->ProductID'>Delete</a>
+		</div>
+		</div>";
+	}
+	?>
+
+
+	
+	
+
+</div>
+<?php
+if(isset($_GET['delete_id'])){
+	
+	$songID=$_GET['delete_id'];
+	
+	$result2=$conn->query("delete from product where ProductID=$songID");
+	
+	
+	if($result2){
+		echo "<script>alert('xóa dữ liệu thành công')</script>";
+		echo"<script>window.open('view.php','_self')</script>";
+		
+	}
+	else{
+		echo "<script>alert('xóa dữ liệu thất bại')</script>";
+		
+	}
+}
+
 
 
 
