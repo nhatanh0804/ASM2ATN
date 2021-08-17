5180cafd0d5add973cdb83f8a2f985f44fea4911
Nguyen Nhat Anh <anhnnbhaf190127@fpt.edu.vn>
2021-08-17 14:50:47 +0700

    git init

--- Delete.php
+++ Delete.php
@@ -4,20 +4,25 @@
 	<title></title>
 </head>
 <body>
-<?php
-include ('include/connect.php');
-if(isset($_REQUEST['id']) and $_REQUEST['id']!=""){
-$SongID=$_GET['id'];
-$sql = "DELETE FROM product WHERE ProductID='$SongID'";
-if ($connect->query($sql) === TRUE) {
-echo "Xoá thành công!";
-} else {
-echo "Error updating record: " . 
-$connect->error;
-}
- 
-$connect->close();
-}
-?>
+	<?php
+	include("connect.php");
+	if (!$connect){
+		echo "kết nối thất bại";
+	}else{
+		echo "";
+	}
+	if(isset($_REQUEST['id']) and $_REQUEST['id']!=""){
+		$SongID=$_GET['id'];
+		$sql = "DELETE FROM product WHERE ProductID='$SongID'";
+		if ($connect->query($sql) === TRUE) {
+			echo "Xoá thành công!";
+		} else {
+			echo "Error updating record: " . 
+			$connect->error;
+		}
+		
+		$connect->close();
+	}
+	?>
 </body>
 </html>
